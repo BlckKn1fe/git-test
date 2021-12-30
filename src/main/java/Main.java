870a802acca3d16558446fe4dbe3c07f7e29a8de
BlackKnife <lding1003@gmail.com>
2021-12-30 08:16:53 -0800

    5th commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello Git 2");
         System.out.println("Hello Git 3");
         System.out.println("Hello Git 4");
+        System.out.println("Hello Git 5");
     }
 
 }
